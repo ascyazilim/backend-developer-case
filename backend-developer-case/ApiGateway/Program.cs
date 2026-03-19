93eb163ae434f23ae75be933e151997b38c2a8b2
ascyazilim <yazilimasc@gmail.com>
2026-03-19 16:51:44 +0300

    feat: implement IP-based rate limiting in API Gateway
    - Added IP-based rate limiting policy using Microsoft.AspNetCore.RateLimiting
    - Configured YARP to enforce rate limits to prevent abuse and DDoS attacks
    - Secured middleware pipeline with correct Routing and RateLimiter order

--- backend-developer-case/ApiGateway/Program.cs
+++ backend-developer-case/ApiGateway/Program.cs
@@ -1,12 +1,36 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// YARP servisini ekliyor ve ayarları appsettings.json içindeki "ReverseProxy" bölümünden okumasını söylüyoruz
+//HIZ SINIRI (IP TABANLI)
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    // "KatiKural" adında, kullanıcının IP adresine göre çalışan bir politika (Policy) yazıyoruz
+    options.AddPolicy("KatiKural", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            // İstek atan kişinin IP adresini alıyoruz. Bulamazsa "unknown" diyoruz.
+            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: partition => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 20, // 10 saniyede sadece 20 istek hakkı
+                Window = TimeSpan.FromSeconds(10),
+                QueueLimit = 0 // Sıraya alma, anında 429 hatası fırlat
+            }));
+});
+
+// YARP Ayarları
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-// Gelen HTTP isteklerini YARP'a yönlendiriyoruz
+app.UseRouting();
+app.UseRateLimiter(); // Polis IP'yi kontrol edecek
 app.MapReverseProxy();
+
+
 
 app.Run();
